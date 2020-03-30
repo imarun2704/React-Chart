53490342cd1d34f3f30e4d2899342400e7086234
Arun <arunkumar@haplabz.com>
2020-03-30 21:42:51 +0530

    wrapped all charts into one chart

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -21,9 +21,10 @@ class Chart extends React.Component {
   fetch = () => {
     axios
       .get(
-        " https://api.thingspeak.com/channels/1023166/feeds.json?api_key=A3EHCB68NAMPXWFD&results="
+        "https://api.thingspeak.com/channels/1023166/feeds.json?api_key=A3EHCB68NAMPXWFD&results="
       )
       .then(data => {
+          console.log('mmmmmmmmmmmmmmmmm')
         this.props.setLineData();
         this.setState({ click: true });
 
@@ -43,95 +44,46 @@ class Chart extends React.Component {
 
         const data1 = {
           labels: field1Lable,
-          datasets: [{
-            label: 'Sales',
-            type:'line',
-            data: this.props.pressureData,
+          datasets: [ {
+              
+            label: "temperature",
+            type:"bar",
             fill: false,
-            borderColor: '#EC932F',
-            backgroundColor: '#EC932F',
-            pointBorderColor: '#EC932F',
-            pointBackgroundColor: '#EC932F',
-            pointHoverBackgroundColor: '#EC932F',
-            pointHoverBorderColor: '#EC932F',
-            yAxisID: 'y-axis-2'
+            backgroundColor: "rgb(0,0,128)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 1,
+            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: field2,
+            yAxisID: 'y-axis-1'
           },
             {
               label: "Pressure",
+              type:"line",
+              fill: false,
               backgroundColor: "rgba(255,99,132,0.2)",
               borderColor: "rgba(255,99,132,1)",
               borderWidth: 1,
               hoverBackgroundColor: "rgba(255,99,132,0.4)",
               hoverBorderColor: "rgba(255,99,132,1)",
               data: field1
-            }
+            }, {
+                label: "energy",
+                type:"line",
+                fill: false,
+                backgroundColor: "rgb(255,255,0)",
+                borderColor: "rgba(255,99,132,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                hoverBorderColor: "rgba(255,99,132,1)",
+                data: field3
+              }
           ]
         };
 
         this.setState({ data: data1 });
 
-        const data2 = {
-          labels: field1Lable,
-          datasets: [{
-            label: 'Sales',
-            type:'line',
-            data: this.props.temperatureData,
-            fill: false,
-            borderColor: '#EC932F',
-            backgroundColor: '#EC932F',
-            pointBorderColor: '#EC932F',
-            pointBackgroundColor: '#EC932F',
-            pointHoverBackgroundColor: '#EC932F',
-            pointHoverBorderColor: '#EC932F',
-            yAxisID: 'y-axis-2'
-          },
-            {
-              
-              label: "temperature",
-              type:"bar",
-              fill: false,
-              backgroundColor: "rgb(0,0,128)",
-              borderColor: "rgba(255,99,132,1)",
-              borderWidth: 1,
-              hoverBackgroundColor: "rgba(255,99,132,0.4)",
-              hoverBorderColor: "rgba(255,99,132,1)",
-              data: field2,
-              yAxisID: 'y-axis-1'
-            }
-          ]
-        };
-
-        this.setState({ data2: data2 });
-
-        const data3 = {
-          labels: field1Lable,
-          datasets: [{
-            label: 'Sales',
-            type:'line',
-            data: this.props.energyData,
-            fill: false,
-            borderColor: '#EC932F',
-            backgroundColor: '#EC932F',
-            pointBorderColor: '#EC932F',
-            pointBackgroundColor: '#EC932F',
-            pointHoverBackgroundColor: '#EC932F',
-            pointHoverBorderColor: '#EC932F',
-            yAxisID: 'y-axis-2'
-          },
-            {
-              label: "energy",
-              backgroundColor: "rgb(255,255,0)",
-              borderColor: "rgba(255,99,132,1)",
-              borderWidth: 1,
-              hoverBackgroundColor: "rgba(255,99,132,0.4)",
-              hoverBorderColor: "rgba(255,99,132,1)",
-              data: field3
-            }
-          ]
-        };
-
-        this.setState({ data3: data3 });
-      });
+         });
   };
 
 
@@ -150,25 +102,7 @@ class Chart extends React.Component {
                 />
                 )}
                 </div>
-
-                <div className="col-md-4"> 
-                {this.state.data2.labels && (
-                    <Bar
-                        data={ this.state.data2}
-                        width={50}
-                        height={50}
-                    />
-                    )}
-                    </div>
-                    <div className="col-md-4"> 
-                {this.state.data3.labels && (
-                  <Bar
-                    data={this.state.data3}
-                    width={50}
-                    height={50}
-                  />
-                )}
-                </div>
+               
       </div>
     );
   }
